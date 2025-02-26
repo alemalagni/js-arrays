71496ed658760b1dddb278b39882a96a472cf3f8
alemalagni <alemalagni@gmail.com>
2025-02-26 14:22:47 +0100

    fix: JS

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -11,9 +11,10 @@ const teachers = [
 // 1. Utilizzando un indice sull'array teachers, cambia il valore della
 // variabile fourthTeacher qui sotto in modo che sia il quarto insegnante elencato nell'array
 const fourthTeacher = null;
-console.log(teachers[3])
+fourthTeacher = teachers[3]
 
 // 2. Sostituisci il quinto insegnante nell'array teachers con 'Patrick'
+teachers[4] = 'Patrick'
 
 // 3. Rimuovi l'ultimo insegnante dall'array e salvalo nella variabile lastTeacher qui sotto
 const lastTeacher = null;
